efae23260ab9c9080af7c48cfb20bde6bdae1d6d
Ethan Wong <e1hanw0ng@gmail.com>
2022-07-02 23:46:04 +0800

    Update all dependencies and remove unneeded packages, drop IE support.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  extends: ['plugin:vue/essential', '@vue/eslint-config-prettier', '@vue/eslint-config-typescript'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
   },
 }

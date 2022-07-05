244f0a5f6d7170b192de24593bd5ce1a92631981
Ethan Wong <e1hanw0ng@gmail.com>
2022-07-05 22:11:04 +0800

    Add iconfont.

---
+++ src/components/Iconfont.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <span :class="['iconfont', `icon${name ? `-${name}` : ''}`]" :style="{ fontSize: fontSize }" />
+</template>
+
+<script lang="ts">
+export default {
+  props: { name: String, fontSize: String },
+}
+</script>
+
+<style scoped>
+@import '@/styles/iconfont.css';
+</style>

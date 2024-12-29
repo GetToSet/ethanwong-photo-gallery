5d31230a04f05b13bea7ee4917a99cfe83069262
Ethan Wong <e1hanw0ng@gmail.com>
2024-12-29 11:39:15 +0800

    Revert "Add cusdis comment system."
    This reverts commit c70db37966a7fe6506eb71d724ee43a5777504d3.
    
    Removes the comment system until we could find a more reliable approach.

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-declare global {
-  interface Window {
-    CUSDIS: any
-  }
-}
-
 Vue.config.productionTip = false
 
 new Vue({
